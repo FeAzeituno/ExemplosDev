6c8405825c52ef52bc9524a99d39ac3c42d2432a
FeAzeituno <fernando.azeituno@gmail.com>
2017-05-31 13:46:56 -0300

    Upload Exemplo Injeção de Dependências AspNet.Core
    Commit inicial

---
+++ ExemploInjDependencia/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exemploinjdependencia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
